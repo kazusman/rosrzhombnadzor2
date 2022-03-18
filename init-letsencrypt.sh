cf9af7fcc6dd9825086c7b5935c8505f84253f89
Pavel Anokhin <pavel.anokhin@xpate.com>
2022-03-18 16:27:12 +0300

    new url

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(launchbot.ru www.launchbot.ru)
+domains=(rosrzhombnadzor.com www.rosrzhombnadzor.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="p.a.anokhin@gmail.com"
