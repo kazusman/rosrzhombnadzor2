38809761109c12c52a81e17e4edd43f3ba3ad461
Pavel Anokhin <pavel.anokhin@xpate.com>
2022-04-08 18:42:02 +0300

    add redirect to admin page

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,11 @@ from django.urls import include
 from django.urls import path
 
 from app.views import health_check
+from app.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("bot/", include("bot.urls")),
     path("healthcheck/", health_check),
+    path("", index)
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 
-def health_check(_):
+def health_check(_) -> HttpResponse:
 
     return HttpResponse(status=200)
+
+
+def index(_) -> HttpResponseRedirect:
+
+    return HttpResponseRedirect('/admin/')
